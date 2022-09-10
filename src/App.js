7a6298b4b327580b91b7bc3e81215d49841daa55
vinothsmart <s.vinothkanna90@gmail.com>
2022-09-10 11:36:14 +0530

    increment and decrement added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useCallback } from "react";
 import { useSelector } from "react-redux";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const increment = useCallback(() => {}, []);
+  const decrement = useCallback(() => {}, []);
+
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
