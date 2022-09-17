c8b67ba49c26be936f74d03aab634dcd116fe7a5
vinothsmart <s.vinothkanna90@gmail.com>
2022-09-17 19:45:18 +0530

    listing cart items

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Products from "./Products";
 import "./Layout.css";
+import CartItems from "./CartItems";
 const Layout = () => {
   let total = 100;
 
@@ -10,6 +11,7 @@ const Layout = () => {
       <div className="layout">
         <Header />
         <Products />
+        <CartItems />
         <div className="total-price">
           <h3>Total: ${total}</h3>
           <button className="orderBtn">Place Order</button>
