84302d563e3a18f478f948592bbfc66940cb1bad
vinothsmart <s.vinothkanna90@gmail.com>
2022-09-22 18:51:46 +0530

    fecth data using slice

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import Notification from "./components/Notification";
-import { uiActions } from "./store/uiSlice";
+import { sendCartData } from "./store/cartSlice";
 
 let isFirstRender = true;
 
@@ -19,42 +19,7 @@ function App() {
       isFirstRender = false;
       return;
     }
-    const sendRquest = async () => {
-      // Send state as Sending Request
-      dispatch(
-        uiActions.showNotification({
-          open: true,
-          message: "Sending Request",
-          type: "warning",
-        })
-      );
-      const res = await fetch(
-        "https://redux-http-bc782-default-rtdb.firebaseio.com/cartItems.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      await res.json();
-      // Send state as Request is Successful
-      dispatch(
-        uiActions.showNotification({
-          open: true,
-          message: "Sent Request To Database Successfully",
-          type: "success",
-        })
-      );
-    };
-    sendRquest().catch((_err) => {
-      // Send state as Request Failed
-      dispatch(
-        uiActions.showNotification({
-          open: true,
-          message: "Sending Request Failed",
-          type: "error",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./uiSlice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -41,6 +42,49 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        open: true,
+        message: "Sending Request",
+        type: "warning",
+      })
+    );
+    const sendRequest = async () => {
+      // Send state as Sending Request
+      const res = await fetch(
+        "https://redux-http-bc782-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      await res.json();
+      // Send state as Request is Successful
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sent Request To Database Successfully",
+          type: "success",
+        })
+      );
+    };
+    try {
+      await sendRequest();
+    } catch (err) {
+      // Send state as Request Failed
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending Request Failed",
+          type: "error",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
