e95160212bca46b2e5782379ff2b6b990410b0db
vinothsmart <s.vinothkanna90@gmail.com>
2022-09-22 06:27:14 +0530

    updating cart values using firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.table(cartItems);
+
+  useEffect(() => {
+    fetch(
+      "https://redux-http-bc782-default-rtdb.firebaseio.com/cartItems.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
